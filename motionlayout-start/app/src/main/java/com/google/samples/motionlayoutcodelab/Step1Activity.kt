a241fdce959af648298d2fe4d05dce02af90203f
takumi-saito <takumi.saitoh.dev@gmail.com>
2021-03-30 05:38:44 +0900

    finish step 1

--- motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step1Activity.kt
+++ motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step1Activity.kt
@@ -16,11 +16,7 @@
 package com.google.samples.motionlayoutcodelab
 
 import android.os.Bundle
-import android.view.View
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
-
-import kotlinx.android.synthetic.main.activity_step1.*
 
 class Step1Activity : AppCompatActivity() {
 
